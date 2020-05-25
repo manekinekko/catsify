0939a009aedbef43ac07268c1421ce9ffa31d0f5
Wassim Chegham <github@wassim.dev>
2020-05-25 16:26:32 +0200

    fix: give Oryx regular files to work on

--- scripts/build.linux.sh
+++ scripts/build.linux.sh
@@ -11,3 +11,7 @@ npm install
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 npm run build
+
+# Note: Oryx skips non-regular (symlinks) files. Give it regular files!
+mkdir -p dist/func
+cp bazel-dist/bin/func/*.* dist/func
