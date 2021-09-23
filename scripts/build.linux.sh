8ca2a63fc221b59dc03191b81be0a3f621cfddb6
Wassim Chegham <github@wassim.dev>
2021-09-23 23:04:10 +0200

    chore: debug failing build

--- scripts/build.linux.sh
+++ scripts/build.linux.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-tree -L 4
-
 CI=true
 api="$(pwd)/api"
 
@@ -18,6 +16,7 @@ npm run build
 rm -fr dist/func
 
 mkdir -p dist/func
-tree -L 4
-find bazel-dist/bin/func -name '*.js' -exec cp "{}" dist/func/ \;
-find bazel-dist/bin/rust -name '*.wasm' -exec cp "{}" dist/func/ \;
+tree -L 4 -I 'node_modules|.git'
+
+find dist/bin/func -name '*.js' -exec cp "{}" dist/func/ \;
+find dist/bin/rust -name '*.wasm' -exec cp "{}" dist/func/ \;
